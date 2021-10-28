6abe072598c659246843eb90634486f8a0dee88f
Maxim <mx.pnk@ya.ru>
2021-10-28 17:43:53 +0300

    refactor architecture and coordinates

--- desktop/src/ru/pankov/desktop/DesktopLauncher.java
+++ desktop/src/ru/pankov/desktop/DesktopLauncher.java
@@ -9,8 +9,8 @@ public class DesktopLauncher {
 
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 400;
-		config.height = 700;
+		config.width = 450;
+		config.height = 800;
 		config.resizable = false;
 
 		new LwjglApplication(new Application(), config);
